42360584e423f2ccef0d47c9a3b82d6db2970e10
Jason Yuan <jasonyuan0130@gmail.com>
2024-02-04 20:59:22 +1030

    structure setup

---
+++ admin/src/main/java/com/yiyuandev/abitoflink/ABitOfLinkAdminApplication.java
@@ -0,0 +1,11 @@
+package com.yiyuandev.abitoflink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ABitOfLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ABitOfLinkAdminApplication.class, args);
+    }
+}
