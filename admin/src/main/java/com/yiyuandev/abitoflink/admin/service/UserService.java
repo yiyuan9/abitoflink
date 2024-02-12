9a2dfdb1e28f5c06c601c3b4903d39099372ca5c
Jason Yuan <jasonyuan0130@gmail.com>
2024-02-12 21:44:21 +1030

    feat:user register with bloom filter implementation and username availability check

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/service/UserService.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/service/UserService.java
@@ -2,6 +2,7 @@ package com.yiyuandev.abitoflink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.yiyuandev.abitoflink.admin.dao.entity.UserDO;
+import com.yiyuandev.abitoflink.admin.dto.req.UserRegisterReqDTO;
 import com.yiyuandev.abitoflink.admin.dto.resp.UserRespDTO;
 
 
@@ -13,4 +14,15 @@ public interface UserService extends IService<UserDO> {
      * @return UserRespDTO
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     * check if username has already registered
+     * @return true: registered, false: unregistered
+     */
+    Boolean isUsernameAvailable(String username);
+
+    /**
+     * user registration
+     */
+    void register(UserRegisterReqDTO requestParam);
 }
