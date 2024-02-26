3304a3302f3ece571d9126d84fda0d8f0e1bc334
Jason Yuan <jasonyuan0130@gmail.com>
2024-02-26 21:26:36 +1030

    optimize:avoid cache penetration when short link redirects

--- project/src/main/java/com/yiyuandev/abitoflink/project/common/constant/RedisKeyConstant.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/common/constant/RedisKeyConstant.java
@@ -8,4 +8,6 @@ public class RedisKeyConstant {
     public static final String GOTO_SHORT_LINK_KEY = "short_link_goto_%s";
 
     public static final String LOCK_GOTO_SHORT_LINK_KEY = "short_link_lock_goto_%s";
+
+    public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "short_link_is_null_goto_%s";
 }
--- project/src/main/java/com/yiyuandev/abitoflink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/service/impl/ShortLinkServiceImpl.java
@@ -40,9 +40,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
-import static com.yiyuandev.abitoflink.project.common.constant.RedisKeyConstant.GOTO_SHORT_LINK_KEY;
-import static com.yiyuandev.abitoflink.project.common.constant.RedisKeyConstant.LOCK_GOTO_SHORT_LINK_KEY;
+import static com.yiyuandev.abitoflink.project.common.constant.RedisKeyConstant.*;
 
 @Slf4j
 @Service
@@ -192,6 +192,17 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
             return;
         }
 
+        boolean contains = shortUriCreateCachePenetrationBloomFilter.contains(fullShortUrl);
+        if (!contains){
+            return;
+        }
+
+        String gotoIsNullShortLink = stringRedisTemplate.opsForValue().get(String.format(GOTO_IS_NULL_SHORT_LINK_KEY, fullShortUrl));
+        if (StrUtil.isNotBlank(gotoIsNullShortLink)){
+            return;
+        }
+
+
         RLock lock = redissonClient.getLock(String.format(LOCK_GOTO_SHORT_LINK_KEY, fullShortUrl));
         lock.lock();
 
@@ -207,6 +218,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
             ShortLinkGotoDO shortLinkGotoDO = shortLinkGotoMapper.selectOne(linkGotoQueryWrapper);
 
             if (shortLinkGotoDO == null){
+                stringRedisTemplate.opsForValue().set(String.format(GOTO_IS_NULL_SHORT_LINK_KEY, fullShortUrl), "-", 30L, TimeUnit.MINUTES);
                 return;
             }
 
