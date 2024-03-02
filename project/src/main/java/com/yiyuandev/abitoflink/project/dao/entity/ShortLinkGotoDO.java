ff43a5dc6b4a5a0653bfe23d0168cd1d40be9252
Jason Yuan <jasonyuan0130@gmail.com>
2024-03-02 23:21:55 +1030

    fix:add tableID to avoid error

--- project/src/main/java/com/yiyuandev/abitoflink/project/dao/entity/ShortLinkGotoDO.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dao/entity/ShortLinkGotoDO.java
@@ -1,5 +1,7 @@
 package com.yiyuandev.abitoflink.project.dao.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +17,7 @@ public class ShortLinkGotoDO {
     /**
      * id
      */
+    @TableId(type = IdType.ASSIGN_ID)
     private Long id;
 
     /**
