53f615acbad2c3ae542ec056795ec0007e418085
Jason Yuan <jasonyuan0130@gmail.com>
2024-03-16 15:30:11 +1030

    optimize:set max number of groups that can be created by a single user

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/common/constant/RedisCacheConstant.java
@@ -2,4 +2,6 @@ package com.yiyuandev.abitoflink.admin.common.constant;
 
 public class RedisCacheConstant {
     public static final String LOCK_USER_REGISTER_KEY = "abitoflink:lock_user-register:";
+    
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group-create:%s";
 }
